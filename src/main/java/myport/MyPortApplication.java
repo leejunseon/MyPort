d63cead82deb84db51cad769619af05eb290e2b1
leejunseon <ljs921026@gmail.com>
2021-09-09 13:22:39 +0900

    20210909

---
+++ src/main/java/myport/MyPortApplication.java
@@ -0,0 +1,13 @@
+package myport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myport/MyPortApplicationTests.java
@@ -0,0 +1,13 @@
+package myport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
