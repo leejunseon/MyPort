a9200fe1d41d5e39365ceed59734a6403781ed59
leejunseon <ljs921026@gmail.com>
2021-10-04 21:53:04 +0900

    20211004

--- src/main/resources/static/js/utils/chart-bar.js
+++ src/main/resources/static/js/utils/chart-bar.js
@@ -3,106 +3,137 @@ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,Bli
 Chart.defaults.global.defaultFontColor = '#858796';
 
 function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-  }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
+	// *     example: number_format(1234.56, 2, ',', ' ');
+	// *     return: '1 234,56'
+	number = (number + '').replace(',', '').replace(' ', '');
+	var n = !isFinite(+number) ? 0 : +number,
+		prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+		sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+		dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+		s = '',
+		toFixedFix = function(n, prec) {
+			var k = Math.pow(10, prec);
+			return '' + Math.round(n * k) / k;
+		};
+	// Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+	if (s[0].length > 3) {
+		s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	}
+	if ((s[1] || '').length < prec) {
+		s[1] = s[1] || '';
+		s[1] += new Array(prec - s[1].length + 1).join('0');
+	}
+	return s.join(dec);
 }
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["KODEX 200TR", "KBSTAR 대형고배당10TR", "SPY", "DIA", "QQQ", "ARKK", "VT", "TIGER 중장기국채", "TLT", "IEF", "SCHP", "BNDW", "BCI", "KRK 금 현물", "달러RP"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [819535, 728410, 1038478, 405836, 877052, 562892, 980799, 728980, 355705, 549185, 445132, 475280, 793113, 432893, 414527],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 1500000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return number_format(value)+'원';
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
-    },
-  }
+	type: 'bar',
+	data: {
+		labels: ["KODEX 200TR", "KBSTAR 대형고배당10TR", "SPY", "DIA", "QQQ" /*"ARKK", "VT", "TIGER 중장기국채", "TLT", "IEF", "SCHP", "BNDW", "BCI", "KRK 금 현물", "달러RP"*/],
+		datasets: [{
+			label: "KODEX 200TR",
+			backgroundColor: "#4e73df",
+			hoverBackgroundColor: "#2e59d9",
+			borderColor: "#4e73df",
+			data: [819535],
+		},{
+			label: "KBSTAR 대형고배당10TR",
+			backgroundColor: "#4e73df",
+			hoverBackgroundColor: "#2e59d9",
+			borderColor: "#4e73df",
+			data: [728410],
+		},{
+			label: "SPY",
+			backgroundColor: "#4e73df",
+			hoverBackgroundColor: "#2e59d9",
+			borderColor: "#4e73df",
+			data: [1038478],
+		},{
+			label: "DIA",
+			backgroundColor: "#4e73df",
+			hoverBackgroundColor: "#2e59d9",
+			borderColor: "#4e73df",
+			data: [405836],
+		},{
+			label: "QQQ",
+			backgroundColor: "#4e73df",
+			hoverBackgroundColor: "#2e59d9",
+			borderColor: "#4e73df",
+			data: [877052],
+		}],
+	},
+	options: {
+		indexAxis: 'y',
+		maintainAspectRatio: false,
+		layout: {
+			padding: {
+				left: 10,
+				right: 25,
+				top: 25,
+				bottom: 0
+			}
+		},
+		scales: {
+			xAxes: [{
+				time: {
+					unit: 'month'
+				},
+				gridLines: {
+					display: false,
+					drawBorder: false
+				},
+				maxBarThickness: 25,
+			}],
+			yAxes: [{
+				ticks: {
+					min: 0,
+					max: 1500000,
+					maxTicksLimit: 5,
+					padding: 10,
+					// Include a dollar sign in the ticks
+					callback: function(value, index, values) {
+						return number_format(value) + '원';
+					}
+				},
+				gridLines: {
+					color: "rgb(234, 236, 244)",
+					zeroLineColor: "rgb(234, 236, 244)",
+					drawBorder: false,
+					borderDash: [2],
+					zeroLineBorderDash: [2]
+				}
+			}],
+		},
+		plugins: {
+			legend: {
+				position: 'right',
+			},
+			title: {
+				display: true,
+				text: 'Chart.js Horizontal Bar Chart'
+			}
+		},
+		tooltips: {
+			titleMarginBottom: 10,
+			titleFontColor: '#6e707e',
+			titleFontSize: 14,
+			backgroundColor: "rgb(255,255,255)",
+			bodyFontColor: "#858796",
+			borderColor: '#dddfeb',
+			borderWidth: 1,
+			xPadding: 15,
+			yPadding: 15,
+			displayColors: false,
+			caretPadding: 10,
+			callbacks: {
+				label: function(tooltipItem, chart) {
+					var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+					return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+				}
+			}
+		},
+	}
 });
